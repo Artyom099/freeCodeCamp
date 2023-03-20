5342835ab9bdbcddd31b8f1e5f139c2eaabbfa4e
Artyom Golubev <artgolubev@bk.ru>
2023-03-20 10:49:33 +0300

    add: 09.js

--- 09.js
+++ 09.js
@@ -68,3 +68,12 @@ function checkEqual(a, b) {
     return a == b ? 'Equal' : 'Not Equal';
 }
 checkEqual(1, 2);
+
+
+
+function checkSign(num) {
+    return (num > 0) ? 'positive'
+    : (num == 0) ? 'zero'
+    : 'negative';
+}
+checkSign(10);
