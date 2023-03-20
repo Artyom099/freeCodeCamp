bbde7cfc0a77d8649406750fd2f47236031ce308
Artyom Golubev <artgolubev@bk.ru>
2023-03-20 10:39:42 +0300

    add: 09.js

--- 08.js
+++ 08.js
@@ -22,3 +22,16 @@ do {
   myArray.push(i);
   i++;
 } while (i < 10)
+
+
+
+function convertToInteger(str) {
+  return parseInt(str);
+}
+convertToInteger("56");
+
+
+function convertToInteger(str) {
+  return parseInt(str, 2);
+}
+convertToInteger("10011");
