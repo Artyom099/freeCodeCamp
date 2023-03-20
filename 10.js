ab08e02fa0ade0df0299fc60037292e78bb94429
Artyom Golubev <artgolubev@bk.ru>
2023-03-20 14:41:06 +0300

    feat: 09.js

--- 10.js
+++ 10.js
@@ -7,3 +7,21 @@ function checkScope() {  // use only 'let'
     console.log('Function scope i is: ', i);
     return i;
 }
+
+
+
+function freezeObj() {
+    const MATH_CONSTANTS = {
+      PI: 3.14
+    };
+    // Only change code below this line
+    Object.freeze(MATH_CONSTANTS);      // ф-я не дает в дальнейшем изменять объект
+    // Only change code above this line
+    try {
+      MATH_CONSTANTS.PI = 99;
+    } catch(ex) {
+      console.log(ex);
+    }
+    return MATH_CONSTANTS.PI;
+  }
+  const PI = freezeObj();
