de0cfb304355ccf35859fe499f5fe13c6d501cbc
Artyom Golubev <artgolubev@bk.ru>
2023-03-20 20:12:20 +0300

    feat: 09.js

--- 10.js
+++ 10.js
@@ -34,3 +34,15 @@ const magic = () => new Date(); // пример стрелочной ф-ии
 
 const myConcat = (arr1, arr2) => arr1.concat(arr2);
 console.log(myConcat([1, 2], [3, 4, 5]));
+
+
+// Only change code below this line
+const increment = (number, value = 1) => number + value;
+// Only change code above this line
+
+
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+let arr2;
+arr2 = [...arr1];  // Change this line
+console.log(arr2);
