d516a74a80562d01997727a11f060a397e5e9604
Artyom Golubev <artgolubev@bk.ru>
2023-03-21 19:33:53 +0300

    add: RegExp.js

--- Regular Expressions.js
+++ Regular Expressions.js
@@ -1,5 +1,5 @@
 // 1
-let myString = "Hello, World!";
+let myString1 = "Hello, World!";
 let myRegex = /Hello/;
 let result1 = myRegex.test(myString);; //    Change this line
 
@@ -7,7 +7,19 @@ let result1 = myRegex.test(myString);; //    Change this line
 // 2
 let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
 let waldoRegex = /Waldo/; // Change this line
-let result = waldoRegex.test(waldoIsHiding);
+let result2 = waldoRegex.test(waldoIsHiding);
 
 
 //3
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result3 = petRegex.test(petString);
+
+
+// 4
+let myString = "freeCodeCamp";
+let fccRegex = /freeCodeCamp/i; // i - позволяет игнорировать высоту шрифта
+let result4 = fccRegex.test(myString);
+
+
+// 5
