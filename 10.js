f9c3094c682df242557d5951586e360c116adb7d
Artyom Golubev <artgolubev@bk.ru>
2023-03-21 16:58:55 +0300

    feat: 10.js

--- 10.js
+++ 10.js
@@ -182,3 +182,22 @@ const makeServerRequest = new Promise((resolve, reject) => {
     reject('Data not received');
   }
 });
+
+
+
+
+// Add the then method to your promise. Use result as the parameter of its callback function and log result to the console.
+const makeServerRequest2 = new Promise((resolve, reject) => {
+  // responseFromServer is set to true to represent a successful response from a server
+  let responseFromServer = true;
+
+  if(responseFromServer) {
+    resolve("We got the data");
+  } else {
+    reject("Data not received");
+  }
+
+  makeServerRequest2.then(result => {  // это надо было дописать
+    console.log(result)
+  })
+});
