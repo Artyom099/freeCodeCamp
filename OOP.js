2b307516b5c61f9b06e47868bcd82d203d7768b4
Artyom Golubev <artgolubev@bk.ru>
2023-03-24 16:56:12 +0300

    feat: OOP.js

--- OOP.js
+++ OOP.js
@@ -13,3 +13,17 @@ Dog.prototype = {
 
 // show this relationship with the isPrototypeOf method:
 Dog.prototype.isPrototypeOf(beagle);
+
+
+
+//
+function Dog(name) {
+    this.name = name;
+}
+let beagle = new Dog("Snoopy");
+Dog.prototype.isPrototypeOf(beagle);  // yields true
+// Fix the code below so that it evaluates to true
+Object.prototype.isPrototypeOf(Dog.prototype);
+
+
+//
