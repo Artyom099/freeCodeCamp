154732c932c3db2a4f81c6a6781821c4f00c5186
Artyom Golubev <artgolubev@bk.ru>
2023-03-24 19:37:13 +0300

    feat: OOP.js

--- OOP.js
+++ OOP.js
@@ -190,4 +190,19 @@ function Bird() {
 
 
 
-//
+// Create a module named funModule to wrap the two mixins isCuteMixin and singMixin.
+// funModule should return an object.
+let funModule = (function() {
+    return {
+        isCuteMixin: function(obj) {
+            obj.isCute = function() {
+                true;
+            };
+        },
+        singMixin: function(obj) {
+                obj.sing = function() {
+            console.log("Singing to an awesome tune");
+            };
+        }
+    }
+})();
