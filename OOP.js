d35dea4ed02552d4d78bff9572d26248eeac3afa
Artyom Golubev <artgolubev@bk.ru>
2023-03-24 17:56:17 +0300

    feat: OOP.js

--- OOP.js
+++ OOP.js
@@ -26,4 +26,37 @@ Dog.prototype.isPrototypeOf(beagle);  // yields true
 Object.prototype.isPrototypeOf(Dog.prototype);
 
 
+
+// Edit the code in the spirit of DRY by moving the eat method to the Animal supertype
+function Cat(name) {
+    this.name = name;
+}
+
+Cat.prototype = {
+    constructor: Cat,
+    // eat: function() {
+    //   console.log("nom nom nom");
+    // }
+};
+
+function Bear(name) {
+    this.name = name;
+}
+
+Bear.prototype = {
+    constructor: Bear,
+    // eat: function() {
+    //   console.log("nom nom nom");
+    // }
+};
+
+function Animal() { }
+
+Animal.prototype = {
+    constructor: Animal,
+    eat: function() {console.log('nom nom nom')}
+};
+
+
+
 //
