32fd1b9c38174490f0ce40b04eedbdef9ee01f9e
Artyom Golubev <artgolubev@bk.ru>
2023-03-24 16:40:28 +0300

    add: OOP.js

--- OOP.js
+++ OOP.js
@@ -4,7 +4,7 @@ function Dog(name) {
 }
 
 Dog.prototype = {
-    // Only change code below this line
+    constructor: Dog,
     numLegs: 4,
     eat: function() {console.log('nom, nom, nom')},
     describe: function() {console.log(`My name is ${this.name}`)}
