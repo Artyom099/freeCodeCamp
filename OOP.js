e93f1601d5af4a8c6ecc3918804fee1d8bdfa845
Artyom Golubev <artgolubev@bk.ru>
2023-03-24 18:53:21 +0300

    feat: OOP.js

--- OOP.js
+++ OOP.js
@@ -152,4 +152,25 @@ console.log(penguin.fly());
 
 
 
+// Create a mixin named glideMixin that defines a method named glide.
+// Then use the glideMixin to give both bird and boat the ability to glide.
+let bird = {
+    name: "Donald",
+    numLegs: 2
+};
+let boat = {
+    name: "Warrior",
+    type: "race-boat"
+};
+// Only change code below this line
+let glideMixin = function(obj) {
+    obj.glide = function() {
+        console.log("Gliding!");
+    };
+};
+glideMixin(bird);
+glideMixin(boat);
+
+
+
 //
