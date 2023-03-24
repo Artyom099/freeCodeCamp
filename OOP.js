50bb8533f6d7b2e6d029b052904f17a7beb25978
Artyom Golubev <artgolubev@bk.ru>
2023-03-24 18:36:25 +0300

    feat: OOP.js

--- OOP.js
+++ OOP.js
@@ -123,4 +123,18 @@ let beagle4 = new Dog();
 
 
 
+// Here's an example of Bird overriding the eat() method inherited from Animal:
+function Animal() { }
+Animal.prototype.eat = function() {
+    return "nom nom nom";
+};
+function Bird() { }
+
+Bird.prototype = Object.create(Animal.prototype);
+Bird.prototype.eat = function() {
+    return "peck peck peck";
+};
+
+
+
 //
