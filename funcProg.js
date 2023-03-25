92ff0bbc9710bcb57cdf34c7319100d574c96566
Artyom Golubev <artgolubev@bk.ru>
2023-03-25 21:29:54 +0300

    feat: funcProg.js_2

--- funcProg.js
+++ funcProg.js
@@ -19,4 +19,34 @@ const tea4TeamFCC = getTea(40);
 
 
 
-// - s
+// - 2
+// Function that returns a string representing a cup of green tea
+const prepareGreenTea = () => 'greenTea';
+// Function that returns a string representing a cup of black tea
+const prepareBlackTea = () => 'blackTea';
+/*
+Given a function (representing the tea type) and number of cups needed, the
+following function returns an array of strings (each representing a cup of
+a specific type of tea).
+*/
+const getTea2 = (prepareTea, numOfCups) => {
+  const teaCups = [];
+
+  for(let cups = 1; cups <= numOfCups; cups += 1) {
+    const teaCup = prepareTea();
+    teaCups.push(teaCup);
+  }
+  return teaCups;
+};
+// Only change code below this line
+const tea4GreenTeamFCC = getTea2(prepareGreenTea, 27);
+const tea4BlackTeamFCC = getTea2(prepareBlackTea, 13);
+// Only change code above this line
+console.log(
+  tea4GreenTeamFCC,
+  tea4BlackTeamFCC
+);
+
+
+
+// 3 -
