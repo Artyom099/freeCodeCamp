166fd78b649eeb1637fa20f24c6b4bffa86c624b
Artyom Golubev <artgolubev@bk.ru>
2023-03-26 14:55:50 +0300

    feat: InterAlgScr.js - solve 1st task

--- InterAlgScr.js
+++ InterAlgScr.js
@@ -1,13 +1,23 @@
+// 1 - summ of all elements in array
 function sumAll(arr) {
-    (arr[0] < arr[1]) ?
-    let x = arr[0], let y = arr[1] :
-    let x = arr[1], let y = arr[0];
+    let x = 0;
+    let y = 0;
+    if (arr[0] < arr[1]) {
+        x = arr[0];
+        y = arr[1];
+    } else {
+        x = arr[1];
+        y = arr[0];
+    }
 
     let summ = 0;
-    for (x; x < y; x++) {
-        summ += x;
+    for (let i = x; i <= y; i++) {
+        summ += i;
     }
     return summ;
 }
+sumAll([1, 4]);
 
-console.log(sumAll([1, 4]));
+
+
+// 2 -
