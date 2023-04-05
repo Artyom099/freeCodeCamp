c8b2d04b241fa59bc7ba4650d75e2726a5c7ac5e
Artyom Golubev <artgolubev@bk.ru>
2023-04-05 21:18:36 +0300

    add: training.ts

--- training.ts
+++ training.ts
@@ -1,12 +1,10 @@
 const videoResolutions = ['P144', 'P240', 'P360', 'P480', 'P720', 'P1080', 'P1440', 'P2160']
-const availableResolutions = ["P144","Invalid","P720"]
+const availableResolutions = ["P144", "P240", "P720"]
 
 //если существующий массив НЕ включается в себя элемент полученного массива ф-я выдает false
 function checkArrayValues(existArray, receivedArray) {
     for( let i of receivedArray) {
-        //if (!Object.values(existArray).includes(i))
         if (!existArray.includes(i)) return false
-
     }
     return true
 }
